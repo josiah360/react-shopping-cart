6a14e4aa7b90aea8ee4fd28947614750c71fd64d
Josiah Olajide <josiaholajide@gmail.com>
2022-08-09 07:19:17 +0100

    Implement cart list styling

--- src/components/Header/Cart/CartList.js
+++ src/components/Header/Cart/CartList.js
@@ -9,6 +9,16 @@ const CartList = () => {
             <ul className={classes['cart-list']}>
                 <CartItem />
                 <CartItem />
+                <div className={classes.confirm}>
+                    <div className={classes.total}>
+                        <h2 className={classes.label}>Total Amount</h2>
+                        <p className={classes['total-price']}>$88.99</p>
+                    </div>
+                    <div className={classes.btns}>
+                        <button type='button'>Close</button>
+                        <button type='button'>Order</button>
+                    </div>
+                </div>
             </ul>
         </div>
     )
