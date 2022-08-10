6acae2b032dd238d888ea86708ae0696b584a8c4
Josiah Olajide <josiaholajide@gmail.com>
2022-08-10 01:10:45 +0100

    Cart context complete

--- src/components/Header/Cart/CartList.js
+++ src/components/Header/Cart/CartList.js
@@ -6,28 +6,29 @@ import CartItem from './CartItem';
 import ConfirmOrder from './confirmOrder';
 import classes from './CartList.module.css'
 
+const Backdrop = (props) => {
+    return <div className={classes.bg} onClick={props.closeCart}></div>
+}
+
 const CartModal = (props) => {
 
     const ctx = useContext(CartContext)
     const { cart } = ctx
     const cartContent = cart.length
     
-
     return (
-        <div className={classes.bg} onClick={props.closeCart}>
-            <ul className={classes['cart-list']}>
-                {cartContent === 0 && <h1>No Items in Cart</h1>}
-                {cartContent > 0 && cart.map((item, index) => 
-                    <CartItem 
-                        key={item.mealId}
-                        index={index}
-                        mealId={item.mealId}
-                    />
-                )}
+        <ul className={classes['cart-list']}>
+            {cartContent === 0 && <h1>No Items in Cart</h1>}
+            {cartContent > 0 && cart.map((item, index) => 
+                <CartItem 
+                    key={item.mealId}
+                    index={index}
+                    mealId={item.mealId}
+                />
+            )}
 
-                <ConfirmOrder closeCart={props.closeCart} />
-            </ul>
-        </div>
+            <ConfirmOrder closeCart={props.closeCart} />
+        </ul>
     )
 }
 
@@ -35,13 +36,8 @@ const CartList = (props) => {
 
     return (
         <React.Fragment>
-            {ReactDOM.createPortal(<CartModal 
-                cart={props.cart}
-                increment={props.increment}
-                decrement={props.decrement}
-                closeCart={props.closeCart}
-            />, 
-            document.getElementById('modal'))}
+            {ReactDOM.createPortal(<Backdrop closeCart={props.closeCart} />, document.getElementById('backdrop') )}
+            {ReactDOM.createPortal(<CartModal closeCart={props.closeCart} />, document.getElementById('modal'))}
         </React.Fragment>
     )
 }
