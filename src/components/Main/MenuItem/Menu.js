6b80e8f323bb742583e8123ea4d002e30f09a42e
Josiah Olajide <josiaholajide@gmail.com>
2022-09-05 20:14:56 +0100

    Refactor conditional statement in menu.js

--- src/components/Main/MenuItem/Menu.js
+++ src/components/Main/MenuItem/Menu.js
@@ -7,19 +7,31 @@ import classes from './Menu.module.css'
 const Menu = () => {
 
     const ctx = useContext(CartContext)
-    const { menu } = ctx
+    const { menu, isLoading, error } = ctx
+
+    let content = <p>Meals are not available</p>
+
+    if(menu.length > 0) {
+        content = menu.map((item, idx) =>
+            <MenuItem 
+                key={item.id}
+                id={item.id}
+                index={idx}
+            />
+        )
+    }
+
+    if(isLoading) {
+        content = <p>Fetching meals...</p>
+    }
+
+    if(error) {
+        content = error
+    }
 
     return (
         <ul className={classes.menu}>
-            {ctx.isLoading && <p>Fetching meals...</p>}
-            {!ctx.isLoading && ctx.error && <p>{ctx.error}</p>}
-            {!ctx.isLoading && !ctx.error && menu.map((item, idx) =>
-                <MenuItem 
-                    key={item.id}
-                    id={item.id}
-                    index={idx}
-                />
-            )}
+            {content}
         </ul>
     )
 }
